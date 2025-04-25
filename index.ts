206ae21764757055b7c67d7a77bafd995e7d8720
princoo <princeineza@gmail.com>
2025-04-25 20:16:10 +0200

    initialize project structure with HTML, CSS, TypeScript, and JavaScript files; add .gitignore and package management

---
+++ index.ts
@@ -0,0 +1,73 @@
+interface Image {
+  id: string;
+  author: string;
+  width: number;
+  height: number;
+  url: string;
+  download_url: string;
+}
+let imageData: Image[] = [];
+fetch("https://picsum.photos/v2/list?page=1").then((response: Response) => {
+  response.json().then((data: Image[]) => {
+    imageData = data;
+    displayData(imageData);
+  });
+});
+
+const lightBoxClose: HTMLElement | null =
+  document.querySelector("#light-box-close");
+function displayData(images: Image[] = []) {
+  const container: HTMLElement | null = document.querySelector(".container");
+  if (!container) return;
+  container.innerHTML = "";
+  if (images.length === 0) {
+    container.innerHTML = "<h1>Empty List</h1>";
+  } else {
+    container.innerHTML = "";
+    const fragmentElement = document.createDocumentFragment();
+    for (const image of images) {
+      const listItem = document.createElement("li");
+      const imageElement = document.createElement("img");
+      const iconElement = document.createElement("img");
+      listItem.className =
+        "relative group mb-3 cursor-pointer hover:scale-105 transition-all duration-300 overflow-hidden";
+      imageElement.className =
+        "object-cover rounded-lg w-full h-full group-hover:blur-sm transition-all duration-300 ease-in-out";
+      iconElement.className =
+        "w-10 h-10 group-hover:top-1/2 absolute -top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-black transition-all duration-300 ease-in-out";
+      imageElement.src = image.download_url;
+      iconElement.src = "./view-svgrepo-com.svg";
+      imageElement.alt = image.author;
+      listItem.addEventListener("click", () => {
+        ligthBoxImage(image);
+      });
+      listItem.appendChild(imageElement);
+      listItem.appendChild(iconElement);
+      fragmentElement.appendChild(listItem);
+    }
+    container.appendChild(fragmentElement);
+  }
+}
+
+function ligthBoxImage(image: Image) {
+  const lightBox: HTMLDivElement | null = document.querySelector("#ligthbox");
+  const lightBoxImage: HTMLImageElement | null =
+    document.querySelector("#ligthbox-image");
+  const lightBoxAuthor: HTMLSpanElement | null =
+    document.querySelector("#ligthbox-title");
+  const lightBoxRedirect: HTMLAnchorElement | null = document.querySelector(
+    "#light-box-download"
+  );
+  if (!lightBox || !lightBoxImage || !lightBoxAuthor || !lightBoxRedirect)
+    return;
+  lightBox.classList.remove("hidden");
+  lightBoxImage.src = image.download_url;
+  lightBoxImage.alt = image.author;
+  lightBoxRedirect.href = image.url;
+}
+
+lightBoxClose?.addEventListener("click", () => {
+  const lightBox: HTMLDivElement | null = document.querySelector("#ligthbox");
+  if (!lightBox) return;
+  lightBox.classList.add("hidden");
+});
