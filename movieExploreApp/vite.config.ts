5f8162c9ec9253950f557d7d2acdafe979244512
princoo <princeineza@gmail.com>
2025-05-11 21:07:40 +0200

    feat: add GitHub Actions workflow for deployment to GitHub Pages

--- movieExploreApp/vite.config.ts
+++ movieExploreApp/vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/week-5/movie-explorer-app/',
   plugins: [react(), tailwindcss()],
 });
