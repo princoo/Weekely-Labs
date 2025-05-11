2d7051715433bab8290c8fe0c9d3a57070975b82
princoo <princeineza@gmail.com>
2025-05-11 21:41:55 +0200

    fix: update base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/lab-5/movie-explore-app/',
+  base: '/Weekly-Labs/lab-5/movie-explore-app/',
   plugins: [react(), tailwindcss()],
 });
