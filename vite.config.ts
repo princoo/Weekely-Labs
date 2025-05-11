a959d0cc4f28bc05fe7777410bd7a45d6becd914
princoo <princeineza@gmail.com>
2025-05-11 22:16:46 +0200

    fix: correct destination directory name in deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Weekly-Labs/lab-5/movie-explorer-app/',
+  base: '/Weekly-Labs/lab-5/movie-explore-app/',
   plugins: [react(), tailwindcss()],
 });
