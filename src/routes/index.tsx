e0911e293bda93077a576d237c3ea8fe1323ff41
princoo <princeineza@gmail.com>
2025-06-02 18:37:11 +0200

    refactor: reorganize router configuration for improved readability and maintainability

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,28 +6,33 @@ import { movieDetailLoader } from "./movieDetailLoader";
 import WatchlistPage from "../Pages/WatchList";
 import Loader from "../components/Loader";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <Hero />,
+        },
+        {
+          path: "movie/:movieId",
+          element: <MoviePage />,
+          loader: movieDetailLoader,
+          errorElement: <div>Could not load movie.</div>,
+          hydrateFallbackElement: <Loader />,
+        },
+        {
+          path: "watchlist",
+          element: <WatchlistPage />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Hero />,
-      },
-      {
-        path: "movie/:movieId",
-        element: <MoviePage />,
-        loader: movieDetailLoader,
-        errorElement: <div>Could not load movie.</div>,
-        hydrateFallbackElement: <Loader/>
-      },
-      {
-        path: "watchlist",
-        element: <WatchlistPage />,
-      },
-    ],
-  },
-]);
+    basename: "/Weekely-Labs/lab-5/movie-explore-app/",
+  }
+);
 
 export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,18 @@ import { defineConfig, loadEnv } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   base: '/Weekely-Labs/lab-5/movie-explore-app/',
-//   plugins: [react(), tailwindcss()],
-// });
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load env file based on the current mode (e.g., development, production)
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    base: env.VITE_APP_BASE_URL || '/',
-    plugins: [react(), tailwindcss()],
-  };
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/Weekely-Labs/lab-5/movie-explore-app/',
+  plugins: [react(), tailwindcss()],
 });
+// https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => {
+//   // Load env file based on the current mode (e.g., development, production)
+//   const env = loadEnv(mode, process.cwd());
+
+//   return {
+//     base: env.VITE_APP_BASE_URL || '/',
+//     plugins: [react(), tailwindcss()],
+//   };
+// });
