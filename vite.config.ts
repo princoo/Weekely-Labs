59916f3b07c9ea5876810cd034825a76c390eaa7
princoo <princeineza@gmail.com>
2025-06-02 18:48:00 +0200

    refactor: remove unused loadEnv import and clean up commented code in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
@@ -7,13 +7,3 @@ export default defineConfig({
   base: '/Weekely-Labs/lab-5/movie-explore-app/',
   plugins: [react(), tailwindcss()],
 });
-// https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => {
-//   // Load env file based on the current mode (e.g., development, production)
-//   const env = loadEnv(mode, process.cwd());
-
-//   return {
-//     base: env.VITE_APP_BASE_URL || '/',
-//     plugins: [react(), tailwindcss()],
-//   };
-// });
